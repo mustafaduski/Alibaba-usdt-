85b30dbc33636fcd11a0784b157c44ddc6f6b50d
mustafaduski <1ksport76@gmail.com>
2026-02-25 21:26:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
-    API_KEY: '8bd9f77f24msh18b6dadd5d8b7f8p17258ajsn5ad908cd38f9',
-    API_HOST: 'alibaba-datahub.p.rapidapi.com',
-    PROFIT_MARGIN: 1.40, // ٤٠٪ قازانج بخەرە سەر هەموو شتێک بە ئۆتۆماتیکی
+    API_KEY: 'ADF5989C318747BD9EAA92F02AAA0C5C', // کلیلی تایبەتی تۆ
+    PROFIT_MARGIN: 1.30, // ٣٠٪ قازانج بخە سەر نرخەکان
     WALLET_ADDR: 'TVWRheHD5zVeXeF3jnWpZrVunaTGLZza3i'
 };

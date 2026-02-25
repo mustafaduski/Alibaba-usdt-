22b4dbbe5a6e0ab485b4844d1bc2113762f6fb5a
mustafaduski <1ksport76@gmail.com>
2026-02-25 18:55:27 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    API_KEY: '8bd9f77f24msh18b6dadd5d8b7f8p17258ajsn5ad908cd38f9',
+    API_HOST: 'alibaba-datahub.p.rapidapi.com',
+    PROFIT_MARGIN: 1.40, // ٤٠٪ قازانج بخەرە سەر هەموو شتێک بە ئۆتۆماتیکی
+    WALLET_ADDR: 'TVWRheHD5zVeXeF3jnWpZrVunaTGLZza3i'
+};

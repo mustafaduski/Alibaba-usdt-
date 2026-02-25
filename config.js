7a1f09f67b352e66d02b062ef16630a6ccdc2059
mustafaduski <1ksport76@gmail.com>
2026-02-25 21:40:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_KEY: 'ADF5989C318747BD9EAA92F02AAA0C5C', // کلیلی تایبەتی تۆ
-    PROFIT_MARGIN: 1.30, // ٣٠٪ قازانج بخە سەر نرخەکان
+    API_KEY: 'ADF5989C318747BD9EAA92F02AAA0C5C',
+    PROFIT_MARGIN: 1.30, 
     WALLET_ADDR: 'TVWRheHD5zVeXeF3jnWpZrVunaTGLZza3i'
 };
